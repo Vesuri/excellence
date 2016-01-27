fb84a492e227331d1edd61fd52f45e48b6038915
Vesa Halttunen <vesuri@jormas.com>
2016-01-27 22:48:15 +0100

    Clean up Bresenham line algorithm

--- algorithms.cpp
+++ algorithms.cpp
@@ -4,56 +4,40 @@
 
 void Algorithms::drawLine(QImage &image, const QPoint &from, const QPoint &to)
 {
-    int CurrentX, CurrentY;
-    int Xinc, Yinc;
-    int Dx, Dy;
-    int TwoDx, TwoDy;
-    int TwoDxAccumulatedError;
-    int TwoDyAccumulatedError;
-
-    Dx = to.x() - from.x();
-    Dy = to.y() - from.y();
-    TwoDx = Dx + Dx;
-    TwoDy = Dy + Dy;
-    CurrentX = from.x();
-    CurrentY = from.y();
-    Xinc = 1;
-    Yinc = 1;
-
-    if (Dx < 0) {
-        Xinc = -1;
-        Dx = -Dx;
-        TwoDx = -TwoDx;
-    }
-    if (Dy < 0) {
-        Yinc = -1;
-        Dy = -Dy;
-        TwoDy = -TwoDy;
-    }
     image.setPixel(from, 1);
 
-    if (Dx != 0 || Dy != 0) {
-        if (Dy <= Dx) {
-            TwoDxAccumulatedError = 0;
-            while (CurrentX != to.x()) {
-                CurrentX += Xinc;
-                TwoDxAccumulatedError += TwoDy;
-                if (TwoDxAccumulatedError > Dx) {
-                    CurrentY += Yinc;
-                    TwoDxAccumulatedError -= TwoDx;
+    int dx = to.x() - from.x();
+    int dy = to.y() - from.y();
+    if (dx != 0 || dy != 0) {
+        int xAdd = dx >= 0 ? 1 : -1;
+        int yAdd = dy >= 0 ? 1 : -1;
+        dx = dx >= 0 ? dx : -dx;
+        dy = dy >= 0 ? dy : -dy;
+        int twoDx = dx + dx;
+        int twoDy = dy + dy;
+
+        int x = from.x();
+        int y = from.y();
+        int error = 0;
+        if (dy <= dx) {
+            while (x != to.x()) {
+                x += xAdd;
+                error += twoDy;
+                if (error > dx) {
+                    y += yAdd;
+                    error -= twoDx;
                 }
-                image.setPixel(CurrentX, CurrentY, 1);
+                image.setPixel(x, y, 1);
             }
         } else {
-            TwoDyAccumulatedError = 0;
-            while (CurrentY != to.y()) {
-                CurrentY += Yinc;
-                TwoDyAccumulatedError += TwoDx;
-                if (TwoDyAccumulatedError > Dy) {
-                    CurrentX += Xinc;
-                    TwoDyAccumulatedError -= TwoDy;
+            while (y != to.y()) {
+                y += yAdd;
+                error += twoDx;
+                if (error > dy) {
+                    x += xAdd;
+                    error -= twoDy;
                 }
-                image.setPixel(CurrentX, CurrentY, 1);
+                image.setPixel(x, y, 1);
             }
         }
     }
