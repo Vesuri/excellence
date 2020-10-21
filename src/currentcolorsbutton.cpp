dcf8772c67950dcc801bee11d789d88ad76e37e9
Vesa Halttunen <vesuri@jormas.com>
2020-10-21 22:30:47 +0200

    Improve main window layout

--- src/currentcolorsbutton.cpp
+++ src/currentcolorsbutton.cpp
@@ -10,21 +10,23 @@ CurrentColorsButton::CurrentColorsButton(QWidget *parent) : QAbstractButton(pare
 
 void CurrentColorsButton::paintEvent(QPaintEvent *event)
 {
+    QRect rect = event->rect().adjusted(0, 0, 1, 1);
+
     QPainter painter;
     painter.begin(this);
 
     QPainterPath path;
-    path.moveTo(event->rect().topLeft());
-    path.lineTo(event->rect().topRight());
-    path.lineTo(event->rect().right(), (event->rect().top() + event->rect().bottom()) / 2);
-    path.lineTo(event->rect().left(), (event->rect().top() + event->rect().bottom()) / 2);
+    path.moveTo(rect.topLeft());
+    path.lineTo(rect.topRight());
+    path.lineTo(rect.right(), (rect.top() + rect.bottom()) / 2);
+    path.lineTo(rect.left(), (rect.top() + rect.bottom()) / 2);
     painter.fillPath(path, paintColor);
 
     path = QPainterPath();
-    path.moveTo(event->rect().left(), (event->rect().top() + event->rect().bottom()) / 2);
-    path.lineTo(event->rect().right(), (event->rect().top() + event->rect().bottom()) / 2);
-    path.lineTo(event->rect().bottomRight());
-    path.lineTo(event->rect().bottomLeft());
+    path.moveTo(rect.left(), (rect.top() + rect.bottom()) / 2);
+    path.lineTo(rect.right(), (rect.top() + rect.bottom()) / 2);
+    path.lineTo(rect.bottomRight());
+    path.lineTo(rect.bottomLeft());
     painter.fillPath(path, eraseColor);
 
     painter.end();
